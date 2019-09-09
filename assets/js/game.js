d7552bb7eb304d12c2bbb1f20dee82db1626aaf2
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-09-08 21:22:17 -0400

    added the reset function and corrected the button value increases

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,19 @@
-   
+ 
+ // Global variables
          var wins = 0;
          var losses = 0;
          var randomNumber;
          var playerScore;
+         var newplayerScore;
          var gameStarted = false;
 
-        //  var winText = document.getElementById("#wins");
-        //  var lossText = document.getElementById('#losses');
-        //  var randonNumText = document.getElementById('#randomNum');
-        //  var playerScoreText = document.getElementsByClassName(".card-text");
-   
+ 
    
    
    $(document).ready(function () {
 
-
+    // Global variables to keep track of game state (wins/losses, currentScore, targetScore)
+    // Functionalities for: random number generation, starting game, and resetting game.
       
 
     function selectNewNumber() {
@@ -25,38 +24,41 @@
 
 
     function crystalVal() {
-        for (var i = 0; i < 5; i++){
+        for (var i = 0; i < 4; i++){
          $("#crystal-"+i).attr("value", (Math.floor(Math.random() *12)+1) );
         }
     };
+
     
+    // crystalVal();
+    // console.log('value of crystal 0', $('#crystal-0').attr("value"))
     
+    function reset() {
+        playerScore = 0;
+        crystalVal();
+        $('#scoreText').html(playerScore);
+
+    }
 
 
     function startGame() {
-        gameStarted = true;                     //sets game status to "started"
-        selectNewNumber();          //select new random number and display it
+        gameStarted = true;                                          //sets game status to "started"
+        selectNewNumber();                                          //select new random number and display it
         $('#randomNum').text("Random Number:  " + randomNumber);
-        crystalVal();                           //reset the crystal values
-        playerScore = 0;                        //returns player score to zero for the new game
-                                           //clear the player score section
-        $('#wins').text("wins: " + wins);              //display the total wins
-        $('#losses').text("losses: " + losses);            //display the total losses
-        playGame();
+        crystalVal();                                               //reset the crystal values
+        playerScore = 0;                                            //returns player score to zero for the new game
+        $('#wins').text("wins: " + wins);                           //display the total wins
+        $('#losses').text("losses: " + losses);                     //display the total losses
+        
     }
 
-    function playGame() {
-        $('.btn').on('click', function() {
-            var value = $(this).val();
-            value = parseInt(value);
-            console.log(value);
-            var newplayerScore = playerScore += value;
-            $('#scoreText').html(newplayerScore);
-            if (newplayerScore < randomNumber) {
-                return;
-            }
-            else if (newplayerScore === randomNumber) {
-                wins++;
+
+    function checkScore() {
+         
+
+             if (playerScore == randomNumber) {
+                $('#scoreText').html(playerScore);
+                wins++; 
                 alert("Winner, Winner!!!");
                 gameStarted = false;
                 var playOn = confirm("Select okay to continue or cancel to end");
@@ -67,28 +69,51 @@
                     return;
                  }
             }
-            else (newplayerScore > randomNumber); {
+            
+            if (playerScore > randomNumber) {
+                console.log('playerscore:', playerScore, 'and random number:', randomNumber);
+                $('#scoreText').html(playerScore);
                 losses++;
-                alert("Sorry. You lose!");
+                alert("Your score is " + playerScore + " Sorry. You lose!");
                 gameStarted = false;
                 var playOn = confirm("Select okay to continue or cancel to end");
                 if (playOn) {
-                    startGame();
+                   startGame();
                 }
                 else (playOn = false); {
+                    reset();
                     return;
                  }
              }
+             
                   
 
-        });
-    }
+        };
+    
+    
+    // Starter and Click handlers for our crystals (a click handler for each, or one that covers all)
+
     
     startGame();
     
-    console.log("Random Number:"+randomNumber);
-    console.log("Player Score:" +playerScore);
-    $('#scoreText').html(playerScore);
+    $('.btn').on('click', function() {
+        var value = $(this).attr('value');
+        console.log('clicked gem\'s value:', value);
+        value = parseInt(value);
+        playerScore += value;
+        $('#scoreText').html(playerScore);
+        console.log('playerscore:', playerScore, 'and random number:', randomNumber);
+        if (playerScore < randomNumber) {
+           return;
+        }
+        else checkScore(playerScore);
+    });
+    
+    console.log("Random Number:"+ randomNumber);
+    console.log("Player Score:" + playerScore);
+    
+    
+        $('#scoreText').html(playerScore);
  
 
 
